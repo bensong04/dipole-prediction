08285d29e232cd15560cf391c8a6f2f3ad8fd061
Ben Song <40846869+bensong04@users.noreply.github.com>
2022-03-01 14:19:57 -0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,7 +1,35 @@
-# atom3d imports
-import atom3d
-from atom3d.models.cnn import CNN3D
+import os
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch import nn
 
-class DipoleModel(CNN3D):
-    def __init__(self, in_dim, box_size, hidden_dim=64, dropout=0.1):
-        super(DipoleModel, self).__init__(in_dim, out_dim=1, box_size=box_size, hidden_dim=hidden_dim, dropout=dropout)
+def features(iterable):
+    prod = 1
+    for i in iterable:
+        if type(prod) != int:
+            raise ValueError("All elements of iterable must be integers")
+        prod *= i
+    return prod
+
+class DipoleModel(nn.Module):
+    def __init__(self, size):
+        f = features(size)
+        super(DipoleModel, self).__init__()
+        self.flatten = nn.Flatten()
+        self.conv_stack = nn.Sequential( \
+            nn.Conv3d(3, 2), \
+            nn.ReLU(), \
+            nn.Dropout(), \
+            nn.Conv3d(2, 1), \
+            nn.ReLU(), \
+            nn.Linear(f,int(f/3)),
+            nn.Tanh(), \
+            nn.Linear(int(f/3), int(f/9)), \
+            nn.Tanh(), \
+            nn.Linear(int(f/9), 1)
+            )
+        def forward(self, x):
+            x = self.flatten(x)
+            logits = self.conv_stack(x)
+            return logits
